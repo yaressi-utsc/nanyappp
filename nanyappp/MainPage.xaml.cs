59b4a3c019720f1955121e2a1427f918caad3b9a
desktop <desktop@DESKTOP-6DFMNTF>
2024-11-22 23:28:19 -0600

    Agregar archivos de proyecto.

---
+++ nanyappp/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace nanyappp
+
+{ 
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        // Método para manejar clic en "Forgot Password?"
+        private async void OnForgotPasswordClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Forgot Password", "This will navigate to password recovery.", "OK");
+        }
+
+        // Método para manejar clic en "Create an account"
+        private async void OnCreateAccountClicked(object sender, EventArgs e)
+        {
+            await DisplayAlert("Create Account", "This will navigate to account creation.", "OK");
+        }
+    }
+}
